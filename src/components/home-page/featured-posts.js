b35e3d24d4a0fdb34810543a113264f467a543bf
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-03 17:31:26 -0600

    adds new components - posts and home-banner

---
+++ src/components/home-page/featured-posts.js
@@ -0,0 +1,9 @@
+import PostsGrid from "../posts/posts-grid";
+import styles from "./featured-posts.module.css";
+
+export const FeaturedPosts = (props) => (
+  <section className={styles.latest}>
+    <h2>Featured Posts</h2>
+    <PostsGrid posts={props.posts} />
+  </section>
+);

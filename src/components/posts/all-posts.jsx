79edf5e07e812f0e0b15e7c42b43df9c23e87384
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-05 18:00:41 -0600

    adds all-posts component

---
+++ src/components/posts/all-posts.jsx
@@ -0,0 +1,9 @@
+import styles from "./all-posts.module.css";
+import PostsGrid from "./posts-grid";
+
+export const AllPosts = (props) => (
+  <section className={styles.posts}>
+    <h1>All Posts</h1>
+    <PostsGrid posts={props.posts} />
+  </section>
+);
